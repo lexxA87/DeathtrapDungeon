7cdefe4c8a20b5186a5a693b2046fcbb355ef45d
Alex Nikonov <60483531+lexxA87@users.noreply.github.com>
2023-04-06 19:05:24 +0400

    Added buttons for directions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,27 +1,44 @@
 using Assets.Scripts;
 using TMPro;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI CardText;
     Card card;
+    private GameObject[] directions;
 
     //private Card card;
     void Start()
     {
-        //Card card = APIHelper.GetCard();
-        //CardText.text = card.Description;
         GetCard();
     }
 
     [ContextMenu("Get Card")]
-    public async void GetCard()
+    public async void GetCard(string id = "1")
     {
         var httpClient = new APIHelper();
 
-        card = await httpClient.GetCardAsync("1");
+        directions = GameObject.FindGameObjectsWithTag("Direction");
+        for (int i = 0; i < directions.Length; i++)
+        {
+            directions[i].SetActive(false);
+        }
+
+        card = await httpClient.GetCardAsync(id);
         CardText.text = card.Description;
+
+        if (card.Directions.Count > 0)
+        {
+            for (int j = 0; j < card.Directions.Count; j++)
+            {
+                string direction = card.Directions[j].NumberCard.ToString();
+                string description = card.Directions[j].Description;
+                directions[j].GetComponentInChildren<TextMeshProUGUI>().text = description;
+                directions[j].GetComponent<Button>().onClick.AddListener(() => GetCard(direction));
+                directions[j].SetActive(true);
+            }
+        }
     }
 }
